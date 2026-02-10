d24d5b9e6a6d73705f6853cd7f065eb3120152dd
Gustavo-Arj <gustavoaraujo8723@gmail.com>
2026-02-10 17:11:15 -0300

    Atualização

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -5,5 +5,33 @@ class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return: Scaffold(
+      body: Padding(
+        padding: const
+EdgeInsets.all(8.0),
+      child: Column(mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const SizedBox(height: 16),
+        const TextField(decoration: InputDecoration(labelText: 'Email')),
+        const TextField(
+          decoration: InputDecoration(labelText: 'Senha'),
+          obscureText: true,
+        ),
+        const SizedBox(height: 20),
+        ElevatedButton(onPressed: (){
+          Navigator.pushReplacementNamed(context, AppRoutes.feed);
+        },
+        child: const Text('Entrar'),
+        ),
+        TextButton(onPressed: (){
+Navigator.pushNamed(context, AppRoutes.cadastro);
+        },
+        child: const Text('Criar conta'),
+        )
+      ],
+      ),
+    ),
+    );
+  }
 }
